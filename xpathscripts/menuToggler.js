da7221bc2b6db84b9bc920615908808e71670aa0
HansHKF <159186072+HansHKF@users.noreply.github.com>
2024-05-16 23:11:33 +0200

    Add files via upload

--- xpathscripts/menuToggler.js
+++ xpathscripts/menuToggler.js
@@ -1,6 +1,6 @@
 //Allows for the closing of the mobile menu on menu item interaction.
 function toggleMenuVisibility() {
-  //looks for the class "toggler" and flips it's state when menu item is pressed 
+  //looks for the class "toggler" and flips it's state when menu item is pressed
   // e.g: if enable then disable it (close menu)
   var toggler = document.querySelector(".toggler");
   if (window.matchMedia("(max-width: 1000px)").matches) {
@@ -10,4 +10,4 @@ function toggleMenuVisibility() {
   }
 }
 
-  
+
